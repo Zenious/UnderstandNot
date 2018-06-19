b16085d2b6504c871c9498a2853629d35b274983
Zenious <7553066+Zenious@users.noreply.github.com>
2018-06-20 05:28:54 +0800

    intergate extractAudio and readJSON to webserver

--- readJSON.py
+++ readJSON.py
@@ -1,51 +1,59 @@
 import json
 import sys
 
-data = {}
-building_block = []
-sentence = ''
-time_sector = 1
-current_time = 0.0
-TIMING = 1
-timing_block = []
-captions = []
-with open('milestone.json','r') as f:
-	data = json.load(f)
+class Transcribe:
+	
+	def __init__(self):
+		self.TIMING = 1
 
-for block in data['results']['items']:
-	if 'end_time' in block:
-		end_time = float(block['end_time'])
-		if end_time > time_sector * TIMING:
-			sentence = ' '.join(building_block)
-			building_block = []
-			output = (current_time, end_time, sentence)
-			captions.append(output)
-			current_time = block['end_time']
-			time_sector = end_time/1 + 1
-	word = block['alternatives'][0]['content']
-	if block['type'] == 'punctuation':
-		building_block[-1] = building_block[-1] + word
-	else: 
-		building_block.append(word)
+	def formatTiming(self, x): 
+		time_str = str(x)
+		time_str = time_str.split('.')
+		minute,sec = divmod(int(time_str[0]),60)
+		hour, minute = divmod(minute, 60)
+		time_str[0] = sec
+		time_str.insert(0, minute)
+		time_str.insert(0, hour)
+		str_format = '{:02}:{:02}:{:02},{:0<3}'.format(time_str[0], time_str[1], time_str[2], time_str[3])
+		return str_format
 
-sentence = ' '.join(building_block)
-output = (current_time, end_time, sentence)
-captions.append(output)
+	def parseOutput(self, json_file):
+		sentence = ''
+		building_block = []
+		current_time = 0.0
+		time_sector = 1
+		captions = []
+		data = {}
+		timing_block = []
 
-def formatTiming(x): 
-	time_str = str(x)
-	time_str = time_str.split('.')
-	minute,sec = divmod(int(time_str[0]),60)
-	hour, minute = divmod(minute, 60)
-	time_str[0] = sec
-	time_str.insert(0, minute)
-	time_str.insert(0, hour)
-	str_format = '{:02}:{:02}:{:02},{:0<3}'.format(time_str[0], time_str[1], time_str[2], time_str[3])
-	return str_format
+		with open(json_file,'r') as f:
+			data = json.load(f)
 
-for x,y in enumerate(captions):
-	print(x+1)
-	str_format = '{} --> {}'.format(formatTiming(y[0]), formatTiming(y[1]))
-	print (str_format)
-	print (y[2])
-	print('')
+		for block in data['results']['items']:
+			if 'end_time' in block:
+				end_time = float(block['end_time'])
+				if end_time > time_sector * self.TIMING:
+					sentence = ' '.join(building_block)
+					building_block = []
+					output = (current_time, end_time, sentence)
+					captions.append(output)
+					current_time = block['end_time']
+					time_sector = end_time/1 + 1
+			word = block['alternatives'][0]['content']
+			if block['type'] == 'punctuation':
+				building_block[-1] = building_block[-1] + word
+			else: 
+				building_block.append(word)
+
+		sentence = ' '.join(building_block)
+		output = (current_time, end_time, sentence)
+		captions.append(output)
+
+		srt_file = json_file + '.srt'
+		with open(srt_file, 'w') as f:
+			for x,y in enumerate(captions):
+				print(x+1, file=f)
+				str_format = '{} --> {}'.format(self.formatTiming(y[0]), self.formatTiming(y[1]))
+				print (str_format, file=f)
+				print (y[2], file=f)
+				print('',file=f)
--- webserver.py
+++ webserver.py
@@ -1,11 +1,74 @@
 from sanic import Sanic
 from sanic import response
-
+from extractAudio import AudioExtract
+from readJSON import Transcribe
 app = Sanic()
+app.static('/static', '.')
 
 @app.route('/')
 async def test(request):
-    return response.text('Hello World')
+    return response.html('''<!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="upload" method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>   
+    <h1>Upload new JSON</h1>
+    <form action="parse" method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+        ''')
 
+
+@app.route('/upload', methods=['POST'])
+async def post_upload(request):
+    if 'file' not in request.files:
+        return response.text('no files')
+    file = request.files['file']
+    file_body = file[0].body
+    file_name = file[0].name
+    file_type = file[0].type
+
+    # check if is valid filetype
+    if 'video' in file_type:
+        # generate db record for index
+
+        create_file(file_name, file_body)
+        # TODO upload video to S3 Bucket
+        audio_file = AudioExtract.extractFLAC(file_name)
+        # TODO upload audio to S3 Bucket
+        # TODO send req to transcribe
+        return response.html('''<!DOCTYPE html>
+<html>
+<body>
+
+<audio controls>
+  <source src="/static/{}" type="audio/flac">
+Your browser does not support the audio element.
+</audio>
+</body>
+</html>
+
+            '''.format(audio_file))
+    else:
+        create_file(file_name, file_body)
+        trans = Transcribe()
+        trans.parseOutput(file_name)
+        return response.text(file_type)
+    # if file.filename == '':
+    #     return response.text('no filename')
+    # return response.text('got found')
+
+def create_file(filename, data):
+    f = open(filename, 'wb')
+    f.write(data)
+    f.close()
+
+@app.route('/parse', methods=['POST'])
+async def post_transcribe(request):
+    Transcribe.parseOutput()
+    return response.text("TODO")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
