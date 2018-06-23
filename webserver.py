2cd5e4e3b74fe9835e0ba93c012cec4e2c7f2f2b
Zenious <7553066+Zenious@users.noreply.github.com>
2018-06-24 02:41:13 +0800

    integrate to aws

--- webserver.py
+++ webserver.py
@@ -2,8 +2,15 @@ from sanic import Sanic
 from sanic import response
 from extractAudio import AudioExtract
 from readJSON import Transcribe
+import uuid 
+import boto3
+import urllib
+
 app = Sanic()
+app.config.REQUEST_MAX_SIZE = 1000000000 # 1GB
 app.static('/static', '.')
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8001")
 
 @app.route('/')
 async def test(request):
@@ -11,7 +18,7 @@ async def test(request):
     <title>Upload new File</title>
     <h1>Upload new File</h1>
     <form action="upload" method=post enctype=multipart/form-data>
-      <input type=file name=file>
+      <input type=file name=file accept="video/*">
       <input type=submit value=Upload>
     </form>   
     <h1>Upload new JSON</h1>
@@ -34,32 +41,39 @@ async def post_upload(request):
     # check if is valid filetype
     if 'video' in file_type:
         # generate db record for index
+        index =  uuid.uuid4().hex
+        table = dynamodb.Table('Videos')
+        table.put_item(Item={
+            'id': index,
+            'title': file_name
+            } )
 
-        create_file(file_name, file_body)
+        create_file(index, file_body)
         # TODO upload video to S3 Bucket
-        audio_file = AudioExtract.extractFLAC(file_name)
+        audio_file = AudioExtract.extractFLAC(index)
         # TODO upload audio to S3 Bucket
+        bucket = 'orbitalphase1'
+        s3 = boto3.client('s3')
+        s3.upload_file(audio_file, bucket, audio_file)
         # TODO send req to transcribe
-        return response.html('''<!DOCTYPE html>
-<html>
-<body>
+        transcribe = boto3.client('transcribe')
 
-<audio controls>
-  <source src="/static/{}" type="audio/flac">
-Your browser does not support the audio element.
-</audio>
-</body>
-</html>
+        result = transcribe.start_transcription_job(
+            TranscriptionJobName=index,
+            LanguageCode='en-US',
+            MediaSampleRateHertz=44100,
+            MediaFormat='flac',
+            Media={
+                'MediaFileUri': 'https://s3-us-east-2.amazonaws.com/{}/{}'.format(bucket,audio_file)
+            }
+        )
+        return response.redirect('/job/{}'.format(index))
 
-            '''.format(audio_file))
     else:
         create_file(file_name, file_body)
         trans = Transcribe()
         trans.parseOutput(file_name)
         return response.text(file_type)
-    # if file.filename == '':
-    #     return response.text('no filename')
-    # return response.text('got found')
 
 def create_file(filename, data):
     f = open(filename, 'wb')
@@ -70,5 +84,38 @@ def create_file(filename, data):
 async def post_transcribe(request):
     Transcribe.parseOutput()
     return response.text("TODO")
+
+@app.route('/job/<id>')
+async def retrieve_job(request, id):
+    transcribe = boto3.client('transcribe')
+    result = transcribe.get_transcription_job(
+        TranscriptionJobName=id)
+
+    status = result['TranscriptionJob']['TranscriptionJobStatus']
+
+    if status == 'COMPLETED':
+        trans_uri = result['TranscriptionJob']['Transcript']['TranscriptFileUri']
+        trans_file = 'trans{}'.format(id)
+        urllib.request.urlretrieve(trans_uri,trans_file)
+        trans = Transcribe()
+        trans.parseOutput(trans_file)
+
+    return response.html('''<!DOCTYPE html>
+<html>
+<body>
+STATUS : {}
+<audio controls>
+  <source src="/static/{}" type="audio/flac">
+Your browser does not support the audio element.
+</audio>
+
+<a href="/static/trans{}.srt">DOWNLOAD LINK</a>
+</body>
+</html>
+
+            '''.format(status,id, id))
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
