7266d444d8f40394ceebefeb2d9bb40ddf7761a8
Zenious <7553066+Zenious@users.noreply.github.com>
2018-06-26 04:52:51 +0800

    enable dynamoDB, srt_to_vtt, job status updated

---
+++ create_table.py
@@ -0,0 +1,27 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.create_table(
+    TableName='Videos',
+    KeySchema=[
+        {
+            'AttributeName': 'id',
+            'KeyType': 'HASH'  #Partition key
+        },
+
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
