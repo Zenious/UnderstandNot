377b073515e93bac156ddfbd594f2eebaa86c444
Zenious <7553066+Zenious@users.noreply.github.com>
2018-07-02 04:17:46 +0800

    reorganisation

--- extractAudio.py
+++ extractAudio.py
@@ -13,8 +13,8 @@ class AudioExtract:
 		basename = os.path.split(base)[0]
 		if basename == "":
 			basename = filename
-		flacname = basename + ".flac"
-		input_video = ffmpeg.input(filename)
-		output_audio = input_video.output(flacname, ac='2', ab='160k', ar='44100')
+		flacname = './resources/' + basename + ".flac"
+		input_video = ffmpeg.input('./resources/{}'.format(filename))
+		output_audio = input_video.output(flacname, ac='1', ab='64k', ar='44100')
 		output_audio.run()
 		return flacname
